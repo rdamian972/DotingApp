1b1213f223ace3829580c5d73593e74d98dfa9e7
Radu Damian <radu.damian@ogilvy.com>
2018-07-24 12:00:11 +0200

    Initial Commit

---
+++ DotingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using DotingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
